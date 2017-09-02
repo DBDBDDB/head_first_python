8a59c2d55a0eedab510e0fd52c5913b944f14036
DBDBDDB <luodianhit@hotmail.com>
2017-09-02 23:39:25 +0800

    1. write the data to database instead of log file 2. debug in attribute of res in cursor.execute. List are now allowed to be sent to datasbase and this can raise for exception here.

--- mymodules/webapp/vsearch4web.py
+++ mymodules/webapp/vsearch4web.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+import mysql.connector
 
 app = Flask(__name__)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent,
-              res, file=log, sep='|')
+    """Log details of the web request and the results."""
+    dbconfig = {'host': '127.0.0.1',
+                'user': 'vsearch',
+                'password': 'vsearchpasswd',
+                'database': 'vsearchlogDB', }
+
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log
+            (phrase, letters, ip, browser_string, results) 
+            values
+            (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL,
+                   (req.form['phrase'],
+                    req.form['letters'],
+                    req.remote_addr,
+                    req.user_agent.browser,
+                    res))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 @app.route('/search4', methods=['POST'])
@@ -15,7 +34,7 @@ def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
-    results = list(search4letters(phrase, letters))
+    results = str(search4letters(phrase, letters))
     log_request(request, results)
     return render_template('results.html',
                            the_title=title,
