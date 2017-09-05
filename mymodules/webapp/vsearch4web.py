4c8c0bfc9771fbffb22431bd4f4083f124fa24f6
DBDBDDB <luodianhit@hotmail.com>
2017-09-05 22:49:38 +0800

    1. create context manager-- DBcm.py 2. using app.config (this is a dictionary built in Flask) 3. update using context manager in vsearch4web.py

--- mymodules/webapp/vsearch4web.py
+++ mymodules/webapp/vsearch4web.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
-import mysql.connector
+
+from DBcm import UseDatabase
 
 app = Flask(__name__)
+
+# app.config is a regular python dictionary built in Flask.
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                          'user': 'vsearch',
+                          'password': 'vsearchpasswd',
+                          'database': 'vsearchlogDB', }
 
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Log details of the web request and the results."""
-    dbconfig = {'host': '127.0.0.1',
-                'user': 'vsearch',
-                'password': 'vsearchpasswd',
-                'database': 'vsearchlogDB', }
 
-    with UseDatabase(dbconfig) as cursor:
+    with UseDatabase(app.config['dbconfig']) as cursor:
         _SQL = """insert into log
                 (phrase, letters, ip, browser_string, results) 
                 values
